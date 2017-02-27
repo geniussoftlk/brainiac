9b5cd7da7c43be3c0a98c1c0c62151c04b2ca7db
Chathu Vishwajith <4evercreations@gmail.com>
2017-02-27 13:25:08 +0530

    Add alternative info JSON path

--- src/main/java/com/officialgenius/brainiac/diodes/WindowsDiode.java
+++ src/main/java/com/officialgenius/brainiac/diodes/WindowsDiode.java
@@ -1,18 +1,16 @@
 /**
- * Copyright 2015 GENIUS SOFT PVT LTD, Sri Lanka
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright 2015 GENIUS SOFT PVT LTD, Sri Lanka Licensed under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package com.officialgenius.brainiac.diodes;
 
 import com.google.gson.Gson;
@@ -28,11 +26,15 @@ import java.nio.file.Paths;
 public class WindowsDiode implements Diodable {
 
     private final String WIN_DROPBOX_INFO_PATH = System.getenv("APPDATA") + "\\Dropbox\\info.json";
+    private final String WIN_DROPBOX_ALTERNATIVE_INFO_PATH = System.getenv("LOCALAPPDATA") + "\\Dropbox\\info.json";
 
     @Override
     public String getDropBoxPath() throws IOException {
         Gson gson = new Gson();
         Path pathToDropBox = Paths.get(WIN_DROPBOX_INFO_PATH);
+        if (!Files.exists(pathToDropBox)) {
+            pathToDropBox = Paths.get(WIN_DROPBOX_ALTERNATIVE_INFO_PATH);
+        }
         BufferedReader br = Files.newBufferedReader(pathToDropBox, Charset.forName("UTF-8"));
         JsonReader jsonReader = new JsonReader(br);
         JsonObject infoJson = gson.fromJson(jsonReader, JsonObject.class);
